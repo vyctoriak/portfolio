a67a6817b59d9263d8b944db0e508fc1867909c8
Vyctoria Karina <vyctoriak@gmail.com>
2025-03-25 20:59:10 -0300

    feat: add spped insights from vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { redirect } from "next/navigation"
 import { defaultLocale } from "@/middleware"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
+  <SpeedInsights/>
   redirect(`/${defaultLocale}`)
 }
 
