21f27ea6c0cc8d5612cbbece15183d2af51c5353
Vyctoria Karina <vyctoriak@gmail.com>
2025-05-03 20:01:38 -0300

    refactor: remove comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProviderWrapper } from '@/components/providers/theme-provider-wrapper';
 
-// Initialize the font outside of the component
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
